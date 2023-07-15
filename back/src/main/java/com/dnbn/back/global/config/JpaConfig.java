aaf68906a19c1d01895690f666839bddebe071ba
junyeobk <73581808+junyeobk@users.noreply.github.com>
2023-07-15 16:58:40 +0900

    Delete back directory

--- back/src/main/java/com/dnbn/back/global/config/JpaConfig.java
+++
@@ -1,9 +0,0 @@
-package com.dnbn.back.global.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@EnableJpaAuditing
-@Configuration
-public class JpaConfig {
-}
--- back/src/main/java/com/dnbn/back/member/entity/Role.java
+++
@@ -1,5 +0,0 @@
-package com.dnbn.back.member.entity;
-
-public enum Role {
-	ROLE_USER, ROLE_ADMIN
-}
